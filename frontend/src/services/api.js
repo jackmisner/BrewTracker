f361aa2a2daae1a5249c5286ec2afd118377e5f4
Jack Misner <jack.misner@gmail.com>
2025-05-22 15:30:10 +0100

    For some reason after an osx update, localhost wasn't being resolved correctly. Fixed by explicitly defining the local ip as 127.0.01

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:5000/api";
 
 // Create axios instance with base URL
 const api = axios.create({
