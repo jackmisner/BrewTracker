debd1367c4c90b846ac89899cbfe10141c8db292
Jack Misner <jack.misner@gmail.com>
2025-06-09 16:52:23 +0100

    ViewBrewSession tests now implemented

--- frontend/src/components/BrewSessions/ViewBrewSession.js
+++ frontend/src/components/BrewSessions/ViewBrewSession.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router";
 import BrewSessionService from "../../services/BrewSessionService";
 import RecipeService from "../../services/RecipeService";
